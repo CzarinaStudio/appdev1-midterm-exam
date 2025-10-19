95bf14196b39f100c246ae40d090d151e1a9586f
Czarina Fombuena <czarinafombuena2019@gmail.com>
2025-10-19 19:40:09 +0800

    Git Hub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+base: "/appdev1-midterm-exam/",
+plugins: [react()],
+});
+
